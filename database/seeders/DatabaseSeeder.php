9a1665d6ee958818a09aab3d07e72ac86b68368b
Luis Buelvas <luisbuelvash0@gmail.com>
2024-05-31 21:26:58 -0500

    seguimiento del taller
    cree un factory, modifique un poco un seeder y instale dos paquetes que son de bootstrap y fontawesome

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        \App\Models\curso::factory(5)->create();
+        
 
-        User::factory()->create([
+       /* User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
-        ]);
+        ]);*/
     }
 }
